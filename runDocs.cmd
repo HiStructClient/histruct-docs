859a2a92b26145ea0bae2270248bcec3089bbf9f
Simon Glanc <simon.glanc@histruct.com>
2023-12-07 09:22:28 +0100

    fix scripts

---
+++ runDocs.cmd
@@ -0,0 +1,2 @@
+
+python -m mkdocs serve -f mkdocs.yml -a localhost:8068
--- runInstructor.cmd
+++ runInstructor.cmd
@@ -1,2 +1,2 @@
 
-mkdocs serve -f mkdocs_plain.yml -a localhost:8067
+python -m mkdocs serve -f mkdocs_plain.yml -a localhost:8067
